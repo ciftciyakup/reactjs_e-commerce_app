108b8323935dca8de3d28b4ee932618138cc4aec
Yakup Ciftci <103890221+ciftciyakup@users.noreply.github.com>
2025-05-31 20:08:28 +0300

    Remove Search

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Navbar from "./components/navbar/Navbar";
 import PageContainer from "./containers/PageContainer";
 import Detail from "./pages/Detail";
 import Cart from "./pages/Cart";
-import Search from "./pages/Search";
+
 function App() {
   return (
     <div>
@@ -15,7 +15,6 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/products/:id" element={<Detail />} />
             <Route path="/cart" element={<Cart />} />
-            <Route path="/products/:search" element={<Search />} />
           </Routes>
         </Router>
       </PageContainer>
